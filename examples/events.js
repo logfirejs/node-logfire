532dfa516f16c36c978c28f386554246a8809fd4
Sascha Gehlich <sascha@gehlich.us>
2014-06-05 23:52:48 +0200

    Added examples, added request dependency

---
+++ examples/events.js
@@ -0,0 +1,18 @@
+'use strict';
+var Logfire = require('logfire-client');
+var client = Logfire.createClient('http://localhost:8085');
+
+client.events.create({
+  event: 'cache.hit',
+  data: {
+    file_type: 'html'
+  }
+}).then(function(id) {
+  console.log('Event created!');
+  client.events.get(id)
+    .then(function (event) {
+      console.log(event);
+    });
+}).catch(function (e) {
+  console.log('Event creation failed:', e.message);
+});
---
+++ examples/queries.js
@@ -0,0 +1,25 @@
+'use strict';
+var Logfire = require('logfire-client');
+var client = Logfire.createClient('http://localhost:8085');
+
+/**
+ * Get the amount of `cache.hit` and `cache.miss` events in the past
+ * 60 minutes grouped by `file_type` where `file_type` is not `html`,
+ * `css` or `js`.
+ */
+client.query({
+  events: ['cache.hit', 'cache.miss'],
+  select: ['$count'],
+  group: 'file_type',
+  start: new Date(Date.now() - (60 * 60 * 1000)),
+  where: {
+    file_type: {
+      $nin: ['html', 'css', 'js']
+    }
+  }
+}).then(function(result) {
+  console.log('Got some results:');
+  console.log(result);
+}).catch(function (e) {
+  console.log('Querying failed:', e.message);
+});
